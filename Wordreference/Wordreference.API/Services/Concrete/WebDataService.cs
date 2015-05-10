62b173406ee79d0b2da7dab5c1b86dde96d4d2ae
Odonno <bottiau.david@laposte.net>
2015-05-10 17:43:52 +0200

    Complete tanslations succesfully for some languages

--- Wordreference/Wordreference.API/Services/Concrete/WebDataService.cs
+++ Wordreference/Wordreference.API/Services/Concrete/WebDataService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -57,49 +56,20 @@ namespace Wordreference.API.Services.Concrete
                     var htmlDocument = new HtmlDocument();
                     htmlDocument.LoadHtml(html);
 
-                    var traductionsRootTable = htmlDocument.DocumentNode.Descendants().FirstOrDefault(d =>
+                    var translationsRootTable = htmlDocument.DocumentNode.Descendants().FirstOrDefault(d =>
                         d.Name == "table" &&
                         d.Attributes["class"] != null &&
                         d.Attributes["class"].Value == "WRD");
 
-                    bool nextTopSection = false;
-
-                    foreach (var translationNode in traductionsRootTable.Descendants().Where(d => d.Name == "tr").Skip(1))
+                    if (translationsRootTable != null)
                     {
-                        if (translationNode.Attributes["class"] != null && translationNode.Attributes["class"].Value == "wrtopsection")
-                            nextTopSection = true;
-
-                        if (translationNode.Attributes["id"] == null)
-                            continue;
-
-                        var translation = RetrieveGridTranslation(translationNode);
-
-                        if (nextTopSection)
-                        {
-                            // Retrieve TraductionsAdditionnelles
-                            Translations.TraductionsAdditionnelles.Add(translation);
-                        }
-                        else
-                        {
-                            // Retrieve TraductionsPrincipales
-                            Translations.TraductionsPrincipales.Add(translation);
-                        }
+                        // Basic Grid search of data
+                        RetrieveGridTanslations(htmlDocument, translationsRootTable);
                     }
-
-                    // Retrieve FormesComposees
-                    var formesComposeesRootTable = htmlDocument.DocumentNode.Descendants().FirstOrDefault(d =>
-                        d.Name == "table" &&
-                        d.Attributes["class"] != null &&
-                        d.Attributes["class"].Value == "WRD" &&
-                        d.Attributes["id"] != null &&
-                        d.Attributes["id"].Value == "compound_forms");
-
-                    foreach (var translationNode in formesComposeesRootTable.Descendants().Where(d => d.Name == "tr").Skip(1))
+                    else
                     {
-                        if (translationNode.Attributes["id"] == null)
-                            continue;
-
-                        Translations.FormesComposees.Add(RetrieveGridTranslation(translationNode));
+                        // Retrieve Roman search of data
+                        RetrieveRomanTranslations(htmlDocument, motRecherche);
                     }
 
                     LanguageDepart = languageDepart;
@@ -114,6 +84,84 @@ namespace Wordreference.API.Services.Concrete
                 return null;
             }
         }
+
+        private void RetrieveGridTanslations(HtmlDocument htmlDocument, HtmlNode translationsRootTable)
+        {
+            bool nextTopSection = false;
+
+            foreach (var translationNode in translationsRootTable.Descendants().Where(d => d.Name == "tr").Skip(1))
+            {
+                if (translationNode.Attributes["class"] != null && translationNode.Attributes["class"].Value == "wrtopsection")
+                    nextTopSection = true;
+
+                if (translationNode.Attributes["id"] == null)
+                    continue;
+
+                var translation = RetrieveGridTranslation(translationNode);
+
+                if (nextTopSection)
+                {
+                    // Retrieve TraductionsAdditionnelles
+                    Translations.TraductionsAdditionnelles.Add(translation);
+                }
+                else
+                {
+                    // Retrieve TraductionsPrincipales
+                    Translations.TraductionsPrincipales.Add(translation);
+                }
+            }
+
+            // Retrieve FormesComposees
+            var formesComposeesRootTable = htmlDocument.DocumentNode.Descendants().FirstOrDefault(d =>
+                d.Name == "table" &&
+                d.Attributes["class"] != null &&
+                d.Attributes["class"].Value == "WRD" &&
+                d.Attributes["id"] != null &&
+                d.Attributes["id"].Value == "compound_forms");
+
+            if (formesComposeesRootTable != null)
+                foreach (var translationNode in formesComposeesRootTable.Descendants().Where(d => d.Name == "tr").Skip(1))
+                {
+                    if (translationNode.Attributes["id"] == null)
+                        continue;
+
+                    Translations.FormesComposees.Add(RetrieveGridTranslation(translationNode));
+                }
+        }
+
+        private void RetrieveRomanTranslations(HtmlDocument htmlDocument, string motRecherche)
+        {
+            foreach (var romanNode in htmlDocument.DocumentNode.Descendants()
+                .Where(d => d.Name == "li" && d.Attributes["class"] != null && d.Attributes["class"].Value == "romanlist"))
+            {
+                foreach (var arabNode in romanNode.Descendants()
+                    .Where(d => d.Name == "li" && d.Attributes["class"] != null && d.Attributes["class"].Value == "arablist"))
+                {
+                    string toTranslation = string.Empty;
+
+                    foreach (var arabNodeChild in arabNode.Descendants()
+                        .Where(d => d.Name == "span" && d.Attributes["class"] != null && !d.Attributes["class"].Value.Contains("headnumber")))
+                    {
+                        toTranslation += WebUtility.HtmlDecode(arabNodeChild.InnerText);
+                    }
+
+                    var translation = new Translation
+                    {
+                        TermeOriginal = new Term
+                        {
+                            Nom = motRecherche
+                        },
+                        FirstTranslation = new Term
+                        {
+                            Nom = toTranslation
+                        }
+                    };
+
+                    Translations.TraductionsPrincipales.Add(translation);
+                }
+            }
+        }
+
 
         private Translation RetrieveGridTranslation(HtmlNode translationNode)
         {
@@ -131,9 +179,9 @@ namespace Wordreference.API.Services.Concrete
                     WebUtility.HtmlDecode(
                         fromWord.Descendants().FirstOrDefault(d => d.Name == "strong").InnerText);
 
-                var typeNode = fromWord.Descendants().FirstOrDefault(d => d.Name == "em").FirstChild;
-                if (typeNode != null)
-                    translation.TermeOriginal.Type = WebUtility.HtmlDecode(typeNode.InnerText);
+                var typeNode = fromWord.Descendants().FirstOrDefault(d => d.Name == "em");
+                if (typeNode != null && typeNode.FirstChild != null)
+                    translation.TermeOriginal.Type = WebUtility.HtmlDecode(typeNode.FirstChild.InnerText);
             }
 
             // Add the translation TO
@@ -146,9 +194,9 @@ namespace Wordreference.API.Services.Concrete
 
                 translation.FirstTranslation.Nom = WebUtility.HtmlDecode(toWord.FirstChild.InnerText);
 
-                var typeNode = toWord.Descendants().FirstOrDefault(d => d.Name == "em").FirstChild;
-                if (typeNode != null)
-                    translation.FirstTranslation.Type = WebUtility.HtmlDecode(typeNode.InnerText);
+                var typeNode = toWord.Descendants().FirstOrDefault(d => d.Name == "em");
+                if (typeNode != null && typeNode.FirstChild != null)
+                    translation.FirstTranslation.Type = WebUtility.HtmlDecode(typeNode.FirstChild.InnerText);
             }
 
 
